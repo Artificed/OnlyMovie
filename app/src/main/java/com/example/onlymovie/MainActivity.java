e1cc5222e6c18fccf111cd56282045037900999b
Artificed <paultsai.id@gmail.com>
2024-10-05 23:13:17 +0700

    feat: Home Navbar and Footer

--- app/src/main/java/com/example/onlymovie/MainActivity.java
+++ app/src/main/java/com/example/onlymovie/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.onlymovie;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -26,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
         setContentView(R.layout.activity_main);
 
         auth = FirebaseAuth.getInstance();
